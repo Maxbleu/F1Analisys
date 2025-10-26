6307c1a18cd89c59edfc2b5c875dc336fac17da8
maxbleu <juanfranciscomm03@gmail.com>
2025-10-26 12:07:56 +0100

    MDF: auth way to verify jwt token

--- utils/auth_utils.py
+++ utils/auth_utils.py
@@ -17,7 +17,11 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
             os.getenv("SECRET_KEY"), 
             algorithms=[os.getenv("ALGORITHM")]
         )
-        if (payload.get("id") != os.getenv("ID_PAYLOAD_JWT") or payload.get("sub") != os.getenv("SUB_PAYLOAD_JWT")):
+        payload_id = str(payload.get("id")).strip().lower()
+        payload_sub = str(payload.get("sub")).strip().lower()
+        expected_id = os.getenv("ID_PAYLOAD_JWT").strip().lower()
+        expected_sub = os.getenv("SUB_PAYLOAD_JWT").strip().lower()
+        if payload_id != expected_id or payload_sub != expected_sub:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, 
                 detail="Token inválido"
